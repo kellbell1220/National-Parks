9be537983acfcd48be94a31ebf1aef7b15b2dced
Kelli Stasiak <30395794+kellbell1220@users.noreply.github.com>
2018-03-12 15:29:25 -0500

    Add files via upload

---
+++ Park.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using static System.Console;
+
+namespace National_Parks
+{
+    class Park
+    {
+        public String parkName;
+        public String location;
+        public String parkType;
+        public String facilities;
+        public int employees;
+        public int annualVisitors;
+        private double budget;
+        private double fee;
+
+        //Constructors
+        public Park()
+        {
+
+        }
+
+        public Park(String parkName, String location, String facilities)
+        {
+            this.parkName = parkName;
+            this.location = location;
+            this.facilities = facilities;
+        }
+
+        public Park(String parkName, String location, double Fee)
+        {
+            this.parkName = parkName;
+            this.location = location;
+            this.Fee = Fee;
+        }
+
+
+        //Private Field for Budget Getter and Setter
+        public double Budget
+        {
+            get
+            {
+                return budget;
+            }
+            set
+            {
+                budget = value;
+            }
+
+        }
+
+        //Private Field for Fee Getter and Setter
+        public double Fee
+        {
+            get
+            {
+                return fee;
+            }
+            set
+            {
+                fee = value;
+            }
+
+        }
+
+
+        //Calculate Price spent on each visitors
+        public double PricePerVisitor()
+        {
+            return Budget / annualVisitors;
+        }
+
+        //Calculating Revenue from fees
+        public double RevenueFromFees()
+        {
+            return annualVisitors * Fee;
+        }
+
+        //ToString method to print out all of the variables and methods
+        public override string ToString()
+        {
+            return "Park: " + parkName + "\nPark Location: " + location + "\nType of Park: " + parkType
+                + "\nFacilities Available: " + facilities + "\nEntrance Fee: " + Fee.ToString("C")
+                + "\nNumber of Employees: " + employees.ToString("N0") + "\nNumber of Visitors Last Year: " + annualVisitors.ToString("N0")
+                + "\nAnnual Budget: " + Budget.ToString("C") + "\nRevenue From Fees: " + RevenueFromFees().ToString("C")
+                + "\nCost Per Visitor: " + PricePerVisitor().ToString("C");
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+
+namespace National_Parks
+{
+    class TestPark
+    {
+        static void Main(string[] args)
+        {
+            Park park1 = new Park();
+            park1.parkName = AquireStringValues("Name: ");
+            park1.location = AquireStringValues("Location: ");
+            park1.parkType = AquireStringValues("Type: ");
+            park1.facilities = AquireStringValues("Facilities Available: ");
+            park1.employees = AquireIntValues("How Many Park Employees: ");
+            park1.annualVisitors = AquireIntValues("How Many Park Annual Visitors: ");
+            park1.Budget = AquireDoubleValues("Annual Budget: ");
+            park1.Fee = AquireDoubleValues("Fee Per Visitor: ");
+
+
+            Park park2 = new Park("Heugonot", "Kerry GA", "Hiking, Biking, Camping");
+
+            Park park3 = new Park("Aster Regional Park", "Maine", 5.00);
+
+            Clear();
+
+            //Printing out park1 object
+            WriteLine(park1.ToString() + "\n\n");
+
+            //Printing out park2 object
+            WriteLine("Park: {0}\nPark Location: {1}\nFacilities Available: {2}\n\n", park2.parkName, park2.location, park2.facilities);
+
+            WriteLine("Park: {0}\nPark Location: {1}\nEntrance Fee: {2:c}", park3.parkName, park3.location, park3.Fee);
+            ReadKey();
+
+        }
+
+        //Asking the user for the string values
+        static public String AquireStringValues(String whichString)
+        {
+            Write("Enter Park {0}", whichString);
+            String inValue = ReadLine();
+
+            return inValue;
+        }
+
+        //Asking the user for int values
+        static public int AquireIntValues(String whichInt)
+        {
+            Write("Enter {0}", whichInt);
+            String inValue = ReadLine();
+
+            return int.Parse(inValue);
+        }
+
+
+        //Asking User For Park Annual Budget
+        static double AquireDoubleValues(String whichDouble)
+        {
+            Write("Enter Park {0}", whichDouble);
+            String inValue = ReadLine();
+
+            return double.Parse(inValue);
+        }
+
+    }
+}
